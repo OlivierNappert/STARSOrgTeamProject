c36cb2c8fe8a3f39854e0d94215d8a7e4e772439
OlivierNappert <olivier.sportif@free.fr>
2017-11-21 18:43:30 -0500

    Member class + stored procedures

---
+++ STARSOrg/cMember.vb
@@ -0,0 +1,128 @@
+﻿Imports System.Data.SqlClient
+
+Public Class cMember
+    'This class represents a single record in the Member Table
+    Private _rstrPID As String
+    Private _rstrFName As String
+    Private _rstrLName As String
+    Private _rstrML As String
+    Private _rstrEmail As String
+    Private _rstrPhone As String
+    Private _rstrPhotoPath As String
+    Private _IsNewMember As Boolean
+
+    'constructor
+    Public Sub New()
+        'initializing
+        _rstrPID = ""
+        _rstrFName = ""
+        _rstrLName = ""
+        _rstrML = Nothing
+        _rstrEmail = ""
+        _rstrPhone = Nothing
+        _rstrPhotoPath = Nothing
+    End Sub
+
+#Region "Exposed properties"
+    Public Property PID As String
+        Get
+            Return _rstrPID
+        End Get
+        Set(strVal As String)
+            _rstrPID = strVal
+        End Set
+    End Property
+
+    Public Property FName As String
+        Get
+            Return _rstrFName
+        End Get
+        Set(strVal As String)
+            _rstrFName = strVal
+        End Set
+    End Property
+
+    Public Property LNAME As String
+        Get
+            Return _rstrLName
+        End Get
+        Set(strVal As String)
+            _rstrLName = strVal
+        End Set
+    End Property
+
+    Public Property ML As String
+        Get
+            Return _rstrML
+        End Get
+        Set(strVal As String)
+            _rstrML = strVal
+        End Set
+    End Property
+
+    Public Property Email As String
+        Get
+            Return _rstrEmail
+        End Get
+        Set(strVal As String)
+            _rstrEmail = strVal
+        End Set
+    End Property
+
+    Public Property Phone As String
+        Get
+            Return _rstrPhone
+        End Get
+        Set(strVal As String)
+            _rstrPhone = strVal
+        End Set
+    End Property
+
+    Public Property PhotoPath As String
+        Get
+            Return _rstrPhotoPath
+        End Get
+        Set(strVal As String)
+            _rstrPhotoPath = strVal
+        End Set
+    End Property
+
+    Public Property IsNewMember As Boolean
+        Get
+            Return _IsNewMember
+        End Get
+        Set(blnVal As Boolean)
+            _IsNewMember = blnVal
+        End Set
+    End Property
+#End Region
+
+    Public ReadOnly Property GetSaveParameters() As ArrayList
+        Get
+            Dim params As New ArrayList
+            params.Add(New SqlParameter("pid", _rstrPID)) 'These are the parameter names that are gonna match our stored procedures
+            params.Add(New SqlParameter("firstName", _rstrFName))
+            params.Add(New SqlParameter("lastName", _rstrLName))
+            params.Add(New SqlParameter("middleName", _rstrML))
+            params.Add(New SqlParameter("email", _rstrEmail))
+            params.Add(New SqlParameter("phone", _rstrPhone))
+            params.Add(New SqlParameter("photoPath", _rstrPhotoPath))
+            params.Add(New SqlParameter("newMember", _IsNewMember))
+            Return params
+        End Get
+    End Property
+
+    Public Function Save() As Integer 'This is a stored procedure
+        'return -1 if the PID already exists
+        If IsNewMember Then
+            Dim params As New ArrayList
+            params.Add(New SqlParameter("pid", _rstrPID))
+            Dim strRes As String = myDB.GetSingleValueFromSP("sp_CheckPIDExists", params)
+            If Not strRes = 0 Then
+                Return -1 'This record already exists
+            End If
+        End If
+        'If not a new member, or it is new and has a unique ID, then do the save (update or insert)
+        Return myDB.ExecSP("sp_SaveMember", GetSaveParameters()) 'Result of 0 means success
+    End Function
+End Class
--- STARSOrg/frmMembers.vb
+++ STARSOrg/frmMembers.vb
@@ -8,6 +8,7 @@ Public Class frmMembers
 
     Private Sub frmMembers_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         myDB = New CDB
+
         If Not myDB.OpenDB() Then
             Application.Exit()
         End If
@@ -24,6 +25,9 @@ Public Class frmMembers
     End Sub
 
     Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        If ModDB.objSQLConn.State = ConnectionState.Open Then
+            myDB.CloseDB()
+        End If
         Me.Hide()
     End Sub
 
@@ -40,5 +44,34 @@ Public Class frmMembers
         Dim tsbProxy As ToolStripButton
         tsbProxy = DirectCast(sender, ToolStripButton)
         tsbProxy.DisplayStyle = ToolStripItemDisplayStyle.Image
+    End Sub
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        Dim blnErrors As Boolean = False
+        Dim params As New ArrayList
+
+        If txtVal.Text.Length = 0 Then
+            errP.SetError(txtVal, "You must enter a search value here")
+            blnErrors = True
+        End If
+
+        If blnErrors Then
+            Exit Sub
+        End If
+
+        Dim strParam As String = "@lastName"
+        Dim strSp As String = "dbo.sp_searchMember"
+
+        params.Add(New SqlParameter(strParam, txtVal.Text))
+        sqlDA = myDB.GetDataAdapterBySp(strSp, params)
+
+        dt = New DataTable
+        sqlDA.Fill(dt)
+        dgrMembers.DataSource = dt
+        dgrMembers.AutoGenerateColumns = True
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+
     End Sub
 End Class
