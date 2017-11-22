2db2f57126a66472b916099cf20e54f576f1b272
Ricky <ricardomsalafia@gmail.com>
2017-11-22 18:40:42 -0500

    added the basic table classes for Security and Audit

---
+++ STARSOrg/cSecurities.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Data.SqlClient
+Public Class cSecurities
+    'Represents the SECURITY table and its associate business rules
+    Private _Security As cSecurity
+    'constructor
+
+    Public Sub New()
+        'insantiate the cSecurity object
+        _Security = New cSecurity
+    End Sub
+
+    Public ReadOnly Property CurrentObject() As cSecurity
+        Get
+            Return _Security
+        End Get
+    End Property
+
+    Public Sub Clear()
+        _Security = New cSecurity
+    End Sub
+    Public Sub CreateNewSecurity()
+        'call this routine when clearing the edit portion of the screen to add a new security
+        Clear()
+        _Security.IsNewSecurity = True
+    End Sub
+    Public Function Save() As Integer
+        Return _Security.Save
+    End Function
+End Class
