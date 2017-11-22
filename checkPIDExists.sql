78fac7ccbc056535dd1506561a1acfa5c04c3703
Ricky <ricardomsalafia@gmail.com>
2017-11-22 17:32:28 -0500

    Added Security classes and Stored Procedure, Still need to complete tableSecurity Class

---
+++ checkPIDExists.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].sp_CheckPIDExists
+	@pID nvarchar(7),
+	@recCount int = 0 Output
+AS
+BEGIN
+Set @recCount=(Select count(0) FROM SECURITY WHERE PID=@pID)
+	SELECT @recCount as RecordCount
+	END
+RETURN 0
---
+++ saveSecurity.sql
@@ -0,0 +1,35 @@
+﻿CREATE PROCEDURE [dbo].sp_SaveSecurity
+@pID nvarchar(7),
+@userID nvarchar(15),
+@password nvarchar(8),
+@secRole nvarchar(10)
+AS
+	Declare @countExists int
+	SELECT @countExists=count(0) FROM SECURITY WHERE PID=@pID
+	if (@countExists=0)
+	BEGIN
+		INSERT INTO SECURITY 
+			(PID
+			,UserID
+			,Password
+			,SecRole
+			)
+			VALUES
+				(@pID
+				,@userID
+				,@password
+				,@secRole
+				)
+
+				END
+				ELSE 
+				BEGIN 
+				UPDATE SECURITY
+				SET 
+				[UserID]=@userID
+				,[Password]=@password
+				,[SecRole]=@secRole
+				WHERE PID= @pID
+
+END
+RETURN @@error
