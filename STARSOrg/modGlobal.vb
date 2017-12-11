8841b3c955f2f1241fc590bf7f2508757f0a4b62
Alejandro Ramirez <arami160@fiu.edu>
2017-12-11 00:18:05 -0600

    njhj

--- STARSOrg/modGlobal.vb
+++ STARSOrg/modGlobal.vb
@@ -19,6 +19,16 @@
 	Public strCurrUserPID As String
 	Public strCurrUserUserID As String
 	Public strCurrUserSecRole As String
+    Public frmMain As Form
+    Public frmMemberRoles As Form
+    Public frmMembers As Form
+    Public frmMembersReport As Form
+    Public frmAdminCHoice As Form
+    Public frmAdminResetPass As Form
+    Public frmEventManger As Form
+    Public frmEventRSVP As Form
+    Public frmLogin As Form
+    Public frmLoginChangePass As Form
 
 
 
