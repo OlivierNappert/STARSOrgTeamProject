1ba86aaa805953197fddbda651849cd8b9094b3c
Kevin Garcia <keving161@gmail.com>
2017-12-10 23:53:48 -0500

    added my event stored procedures

---
+++ STARSOrg/Event Stored Procedures/dbo.sp_CheckEventTypeIDExists.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].sp_CheckEventTypeIDExists
+	@EventTypeID nvarchar(15),
+	@recCount int = 0 OUTPUT
+AS
+BEGIN
+	SET @recCount=(Select count(0) FROM EVENT_TYPE WHERE EventTypeID=@EventTypeID)
+	SELECT @recCount as RecordCount
+END
+RETURN 0
