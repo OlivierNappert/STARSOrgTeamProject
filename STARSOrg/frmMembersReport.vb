114c95019d6927a4665827b150ac0c92ba16cfe6
OlivierNappert <olivier.sportif@free.fr>
2017-12-11 01:10:51 -0500

    Final commit

--- STARSOrg/frmMembersReport.vb
+++ STARSOrg/frmMembersReport.vb
@@ -3,11 +3,10 @@
 Public Class frmMembersReport
 
     Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
-        Me.Hide()
+        Me.Close()
     End Sub
 
     Private Sub frmMembersReport_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'TODO: This line of code loads data into the 'dataSetMembers.MEMBER' table. You can move, or remove it, as needed.
         Me.MEMBERTableAdapter.Fill(Me.dataSetMembers.MEMBER)
 
         Me.ReportViewer1.RefreshReport()
